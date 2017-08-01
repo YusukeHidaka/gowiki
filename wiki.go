b250604bb97b418c27668ba456f2e85d7a80196a
YusukeHidaka <ysk91.akg@gmail.com>
2017-08-01 09:50:40 +0700

    temp save files

--- wiki.go
+++ wiki.go
@@ -32,9 +32,31 @@ func loadPage(title string) (*Page, error) {
   return &Page{Title: title, Body: body}, nil
 }
 
+// エラー処理をしていない例
+// func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+//     t, _ := template.ParseFiles(tmpl + ".html")
+//     t.Execute(w, p)
+// }
+
+// func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+//     t, err := template.ParseFiles(tmpl + ".html")
+//     if err != nil {
+//         http.Error(w, err.Error(), http.StatusInternalServerError)
+//         return
+//     }
+//     err = t.Execute(w, p)
+//     if err != nil {
+//         http.Error(w, err.Error(), http.StatusInternalServerError)
+//     }
+// }
+
+var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
+
 func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
-    t, _ := template.ParseFiles(tmpl + ".html")
-    t.Execute(w, p)
+    err := templates.ExecuteTemplate(w, tmpl+".html", p)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
 
 // テンプレートを使わない
@@ -44,16 +66,19 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 //   fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
 // }
 
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/view/"):]
+func viewHandler(w http.ResponseWriter, r *http.Request, title string) {
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := loadPage(title)
     if err != nil {
-      //リクエストがエラーだった場合
-      http.Redirect(w, r, "/edit/"+title, http.StatusFound)
-      return
+        http.Redirect(w, r, "/edit/"+title, http.StatusFound)
+        return
     }
     renderTemplate(w, "view", p)
 }
+
 // テンプレートを使わない
 // func editHandler(w http.ResponseWriter, r *http.Request) {
 //   title := r.URL.Path[len("/edit/"):]
@@ -69,8 +94,11 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 //     p.Title, p.Title, p.Body)
 // }
 
-func editHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/edit/"):]
+func editHandler(w http.ResponseWriter, r *http.Request, title string) {
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     p, err := loadPage(title)
     if err != nil {
         p = &Page{Title: title}
@@ -78,12 +106,29 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
     renderTemplate(w, "edit", p)
 }
 
-func saveHandler(w http.ResponseWriter, r *http.Request) {
-    title := r.URL.Path[len("/save/"):]
+// エラー処理をしていない
+// func saveHandler(w http.ResponseWriter, r *http.Request) {
+//     title := r.URL.Path[len("/save/"):]
+//     body := r.FormValue("body")
+//     //FormValueの返り値がstringなので、byteに変換している
+//     p := &Page{Title: title, Body: []byte(body)}
+//     p.save()
+//     http.Redirect(w, r, "/view/"+title, http.StatusFound)
+// }
+
+
+func saveHandler(w http.ResponseWriter, r *http.Request, title string) {
+    title, err := getTitle(w, r)
+    if err != nil {
+        return
+    }
     body := r.FormValue("body")
-    //FormValueの返り値がstringなので。
     p := &Page{Title: title, Body: []byte(body)}
-    p.save()
+    err = p.save()
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
     http.Redirect(w, r, "/view/"+title, http.StatusFound)
 }
 
@@ -92,4 +137,17 @@ func main() {
   http.HandleFunc("/edit/", editHandler)
   http.HandleFunc("/save/", saveHandler)
   http.ListenAndServe(":8080", nil)
+}
+
+
+// validation
+var validPath = regexp.MustCompile("^/(edit|save|view)/([a-zA-Z0-9]+)$")
+
+func getTitle(w http.ResponseWriter, r *http.Request) (string, error) {
+    m := validPath.FindStringSubmatch(r.URL.Path)
+    if m == nil {
+        http.NotFound(w, r)
+        return "", errors.New("Invalid Page Title")
+    }
+    return m[2], nil // The title is the second subexpression.
 }
